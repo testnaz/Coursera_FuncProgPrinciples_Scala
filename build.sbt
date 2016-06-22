22b19e809585d13f568a30e7f727b2909c198994
Nazariene <another.nazariene@gmail.com>
2016-06-22 18:44:52 +0300

    Added coursera stuff, week 1-2

---
+++ build.sbt
@@ -0,0 +1,3 @@
+scalaVersion := "2.12.0-M4"
+
+libraryDependencies += "junit" % "junit" % "4.10" % "test"
--- src/ScalaObj.scala
+++ src/ScalaObj.scala
@@ -3,12 +3,19 @@
  */
 object ScalaObj {
 
-            //val main = Main;
+    //val main = Main;
     def main(args: Array[String]): Unit = {
         val numbers = Array(1, 2, 3)
+        //numbers = Array(1,2,2,2); val .. is final
+
         val names = List("Ted", "David", "Someone")
         for (n <- numbers; name <- names) {
             println(n + " " + name)
         }
+
+        var numbers1 = Array(1,2,2)
+        println(numbers1.toString())
+        numbers1 = Array(2,2,2)
+        println(numbers1.toString())
     }
 }
